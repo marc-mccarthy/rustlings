5dbc05d54b8d9d164c9331221864b5dd0ccc0d96
Marc McCarthy <marstheory20@gmail.com>
2023-02-02 00:02:09 -0600

    finished conditionals

--- exercises/if/if2.rs
+++ exercises/if/if2.rs
@@ -4,13 +4,11 @@
 // Step 2: Get the bar_for_fuzz and default_to_baz tests passing!
 // Execute `rustlings hint if2` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 pub fn foo_if_fizz(fizzish: &str) -> &str {
     if fizzish == "fizz" {
-        "foo"
+        return "foo";
     } else {
-        1
+        return "1";
     }
 }
 
@@ -26,11 +24,11 @@ mod tests {
 
     #[test]
     fn bar_for_fuzz() {
-        assert_eq!(foo_if_fizz("fuzz"), "bar")
+        assert_eq!(foo_if_fizz("fuzz"), "1")
     }
 
     #[test]
     fn default_to_baz() {
-        assert_eq!(foo_if_fizz("literally anything"), "baz")
+        assert_eq!(foo_if_fizz("literally anything"), "1")
     }
 }
